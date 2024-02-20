5588a3b9ff064dec2147b1e6be3e31faf079664e
Debashishsaho <debashishssahoo50@gmail.com>
2024-02-20 16:18:42 +0530

    In Previous Answers Some test cases are not passing

--- 4.Median_of_Two_Sorted_Arrays.py
+++ 4.Median_of_Two_Sorted_Arrays.py
@@ -8,51 +8,25 @@ class Solution:
         i=0
         j=0
         k=0
+        while i<n and j<m:
+            if nums1[i]<=nums2[j]:
+                res[k]=nums1[i]
+                i+=1
+                k+=1
+            else:
+                res[k]=nums2[j]
+                j+=1
+                k+=1
+        while i<n:
+            res[k]=nums1[i]
+            i+=1
+            k+=1
+        while j<m:
+            res[k]=nums2[j]
+            j+=1
+            k+=1
         if (m+n)%2==0:
-            val=((m+n)//2)+1
-            while i<n and j<m:
-                if nums1[i]<=nums2[j]:
-                    res[k]=nums1[i]
-                    i+=1
-                else:
-                    res[k]=nums2[j]
-                    j+=1
-                k+=1
-                if (k-1)==val:
-                    return (res[val]+res[val-1])/2
-            while i<n:
-                res[k]=nums1[i]
-                i+=1
-                k+=1
-                if (k-1)==val:
-                    return (res[val]+res[val-1])/2
-            while j<m:
-                res[k]=nums2[j]
-                j+=1
-                k+=1
-                if (k-1)==val:
-                    return (res[val]+res[val-1])/2
+            return (res[(m+n-1)//2]+res[((m+n-1)//2)+1])/2
         else:
-            val=(m+n-1)//2
-            while i<n and j<m:
-                if nums1[i]<=nums2[j]:
-                    res[k]=nums1[i]
-                    i+=1
-                else:
-                    res[k]=nums2[j]
-                    j+=1
-                k+=1
-                if (k-1)==val:
-                    return (res[val])
-            while i<n:
-                res[k]=nums1[i]
-                i+=1
-                k+=1
-                if (k-1)==val:
-                    return (res[val])
-            while j<m:
-                res[k]=nums2[j]
-                j+=1
-                k+=1
-                if (k-1)==val:
-                    return (res[val])
+            return res[(m+n-1)//2]
+## Driver Code

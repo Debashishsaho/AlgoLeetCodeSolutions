c9c714631353d2c6690369360e53a6ad6ee8f47d
Debashishsaho <debashishssahoo50@gmail.com>
2024-02-21 17:15:22 +0530

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ leetcode/1.Two_Sum.py
@@ -0,0 +1,13 @@
+# leetcode Question Two Sumclass Solution:
+# approch : hashing 
+# time complexity:O(n) space complexity:O(n) where n is the length of the nums
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map={}
+        for i in range(len(nums)):
+            if nums[i] in hash_map:
+                return [hash_map[nums[i]],i]
+            hash_map[target-nums[i]]=i
+
+# Driver Code
